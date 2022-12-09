1976cf09095ac334bb8fe5249acef63c520aba4b
teguhdaeli <daeliteguh@gmail.com>
2022-12-10 00:36:08 +0700

    Project NodeJS

--- nodejs-basic/events/index.js
+++ nodejs-basic/events/index.js
@@ -1,3 +1,11 @@
 const {EventEmitter } = require('events');
 
 const myEventEmitter = new EventEmitter();
+
+const birthdayEventListener = (name) => {
+    console.log(`Happy birthday ${name}!`);
+  }
+
+myEventEmitter.on('TodayBirthday', birthdayEventListener);
+
+myEventEmitter.emit('TodayBirthday', 'Teguh');
